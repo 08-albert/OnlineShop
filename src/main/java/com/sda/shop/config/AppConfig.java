56160b0186a7a5b2f4e752b125c614fe162e3607
albert claudiu <albertclaudiu11@yahoo.com>
2020-12-02 17:57:24 +0200

    3rd commit

--- src/main/java/com/sda/shop/config/AppConfig.java
+++ src/main/java/com/sda/shop/config/AppConfig.java
@@ -3,11 +3,10 @@ package com.sda.shop.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ComponentScan("com.sda.shop")
-@Import({DataBaseConfig.class, WebConfig.class})
+@Import({DataBaseConfig.class, WebConfig.class, WebSecurityConfigs.class})
 
 
 public class AppConfig {
